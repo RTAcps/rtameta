d62e752cfb24a45853f16fe2216bc7e86d921bfc
RTAcps <rodrigoandrade20@yahoo.com.br>
2022-07-13 06:41:23 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/rtameta/RtametaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.rtameta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtametaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtametaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/rtameta/RtametaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.rtameta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtametaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
